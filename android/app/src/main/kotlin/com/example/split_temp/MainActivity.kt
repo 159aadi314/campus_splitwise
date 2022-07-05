0b523756d2f915a85d23bb4c12059340c0ead6e6
Siddhu <sidban222@gmail.com>
2022-07-06 00:19:09 +0530

    Revert "login and auth finally"
    This reverts commit 258d31defe9c531112ea4f2ef5e4a807dbaed4a8.

--- android/app/src/main/kotlin/com/example/split_temp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.split_temp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/user.dart
+++
@@ -1,6 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-class myUser {
-  final String uid;
-  myUser({required this.uid});
-}
