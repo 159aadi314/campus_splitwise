5ab5fe64f982238e5bcba47824e909a98f9855f8
aadidell <aadiaarya1@gmail.com>
2022-07-07 23:51:42 +0530

    "Viraj login page UI"

--- lib/src/authenticate/authenticate.dart
+++ lib/src/authenticate/authenticate.dart
@@ -18,68 +18,102 @@ class _AuthenticateState extends State<Authenticate> {
   final DatabaseService _db = DatabaseService();
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Spacer(),
-            Spacer(),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue,
-                onPrimary: Colors.white,
-                minimumSize: Size(double.infinity, 50),
+    return Scaffold(
+      body: Padding(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(),
+              FlutterLogo(size: 120),
+              Spacer(),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Hey there,\nWelcome Back!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontFamily: 'aerial',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-              onPressed: () {
-                Navigator.of(context).pushNamed(Register.id);
-              },
-              child: Text(
-                'Sign Up',
-                style: TextStyle(color: Colors.white),
+              SizedBox(height: 8,),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Login into your Account to continue!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'aerial',
+
+                  ),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue,
-                onPrimary: Colors.white,
-                minimumSize: Size(double.infinity, 50),
+              Spacer(),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 93, 255, 228),
+                  onPrimary: Colors.white,
+                  minimumSize: Size(double.infinity, 50),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushNamed(Register.id);
+                },
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(color: Colors.black,fontSize: 16),
+                ),
               ),
-              onPressed: () {
-                Navigator.of(context).pushNamed(SignIn.id);
-              },
-              child: Text(
-                'Sign In',
-                style: TextStyle(color: Colors.white),
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue,
-                onPrimary: Colors.white,
-                minimumSize: Size(double.infinity, 50),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 93, 255, 228),
+                  onPrimary: Colors.white,
+                  minimumSize: Size(double.infinity, 50),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushNamed(SignIn.id);
+                },
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
               ),
-              icon: FaIcon(FontAwesomeIcons.google),
-              onPressed: () async {
-                dynamic result = await _auth.googleLogIn();
-                await _db.CreateUser(
-                    result.uid, result.email, result.name);
-                if (result == null) {
-                  print("ERROR!!");
-                } else {
-                  print("GR* SUCSES");
-                }
-              },
-              label: Text('Sign In with Google'),
-            ),
-            Spacer(),
-          ],
-        ));
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 93, 255, 228),
+                  onPrimary: Colors.white,
+                  minimumSize: Size(double.infinity, 50),
+                ),
+                icon: FaIcon(FontAwesomeIcons.google, color: Colors.black , size: 24),
+                onPressed: () async {
+                  dynamic result = await _auth.googleLogIn();
+                  await _db.CreateUser(
+                      result.uid, result.email, result.name);
+                  if (result == null) {
+                    print("ERROR!!");
+                  } else {
+                    print("GR* SUCSES");
+                  }
+                },
+                label: Text('Sign In with Google',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Spacer(),
+            ],
+          )),
+    );
   }
 }
