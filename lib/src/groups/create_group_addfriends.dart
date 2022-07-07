cfbf964eb8b90389267325a074fe9a6e1d55510e
Siddhu <sidban222@gmail.com>
2022-07-07 23:43:27 +0530

    Create group button

--- lib/src/groups/create_group_addfriends.dart
+++ lib/src/groups/create_group_addfriends.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:campus_splitwise/src/groups/create_group_confirm.dart';
+
 class AddGroupPage extends StatefulWidget {
   const AddGroupPage({Key? key}) : super(key: key);
 
@@ -8,17 +9,13 @@ class AddGroupPage extends StatefulWidget {
 }
 
 class _AddGroupPage extends State<AddGroupPage> {
-  final List<Map<String,dynamic>> _allfriends = List.generate(20, (index) {
-    return {
-      'id': '$index',
-      'name': 'Friend ${index + 1}',
-      'val': false
-    };
+  final List<Map<String, dynamic>> _allfriends = List.generate(20, (index) {
+    return {'id': '$index', 'name': 'Friend ${index + 1}', 'val': false};
   });
   final _formKey = GlobalKey<FormState>();
 
   List<Map<String, dynamic>> _foundUsers = [];
-  Map<String,dynamic> group_users ={};
+  Map<String, dynamic> group_users = {};
   @override
   initState() {
     // at the beginning, all users are shown
@@ -35,7 +32,7 @@ class _AddGroupPage extends State<AddGroupPage> {
     } else {
       results = _allfriends
           .where((user) =>
-          user["name"].toLowerCase().contains(enteredKeyword.toLowerCase()))
+              user["name"].toLowerCase().contains(enteredKeyword.toLowerCase()))
           .toList();
       // we use the toLowerCase() method to make it case-insensitive
     }
@@ -45,7 +42,8 @@ class _AddGroupPage extends State<AddGroupPage> {
       _foundUsers = results;
     });
   }
-  bool value=false;
+
+  bool value = false;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -60,28 +58,26 @@ class _AddGroupPage extends State<AddGroupPage> {
             ),
           ),
         ),
-    
         body: Padding(
           padding: const EdgeInsets.all(10),
-          child:
-          Column(
+          child: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.fromLTRB(5,5,5,5),
+                padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
                 child: Form(
                   key: _formKey,
                   child: TextFormField(
                     onChanged: (value) => _runFilter(value),
                     decoration: const InputDecoration(
-                        labelText: 'Search friends', suffixIcon: Icon(Icons.search)),
-                    validator: (value){
-                      if(group_users.isEmpty)
+                        labelText: 'Search friends',
+                        suffixIcon: Icon(Icons.search)),
+                    validator: (value) {
+                      if (group_users.isEmpty)
                         return "Please add at least 1 user";
                       return null;
                     },
                   ),
                 ),
-    
               ),
               const SizedBox(
                 height: 20,
@@ -89,19 +85,18 @@ class _AddGroupPage extends State<AddGroupPage> {
               Expanded(
                 child: _foundUsers.isNotEmpty
                     ? ListView.builder(
-                  itemCount: _foundUsers.length,
-                  itemBuilder: (context, index) =>
-                      buildBox(_foundUsers[index]),
-                )
+                        itemCount: _foundUsers.length,
+                        itemBuilder: (context, index) =>
+                            buildBox(_foundUsers[index]),
+                      )
                     : const Text(
-                  'No results found',
-                  style: TextStyle(fontSize: 24),
-                ),
+                        'No results found',
+                        style: TextStyle(fontSize: 24),
+                      ),
               ),
             ],
           ),
         ),
-    
         floatingActionButton: FloatingActionButton(
           heroTag: null,
           onPressed: () {
@@ -114,40 +109,36 @@ class _AddGroupPage extends State<AddGroupPage> {
             }
           },
           // Add your onPressed code here!
-    
-          child: const Icon(Icons.keyboard_arrow_right_rounded, size: 40 ),
+
+          child: const Icon(Icons.keyboard_arrow_right_rounded, size: 40),
         ),
-    
-    
       ),
     );
   }
 
-  Widget buildBox(Map<String,dynamic> friend) => Padding(
-    padding: const EdgeInsets.fromLTRB(4,1,4,1),
-    child: Card(
-      key: ValueKey(friend["id"]),
-      elevation: 2,
-      color: friend['val']?Color.fromARGB(255, 49, 102, 196):null,
-      child: ListTile(
-        visualDensity: VisualDensity.comfortable,
-        // increase size of this icon
-        contentPadding: EdgeInsets.fromLTRB(10, 5, 5, 5),
-        leading:
-        const Icon(Icons.person),
-        title: Text(friend['name'], style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500 )),
-        onTap: () {
-          setState(()=>friend['val']=!friend['val']);
-          if(friend['val'])
-            group_users[friend['id']]=friend['name'];
-          else
-            group_users.remove(friend['id']);
-          // print(group_users);
-
-        },
-      ),
-    ),
-  );
-
+  Widget buildBox(Map<String, dynamic> friend) => Padding(
+        padding: const EdgeInsets.fromLTRB(4, 1, 4, 1),
+        child: Card(
+          key: ValueKey(friend["id"]),
+          elevation: 2,
+          color: friend['val'] ? Color.fromARGB(255, 49, 102, 196) : null,
+          child: ListTile(
+            visualDensity: VisualDensity.comfortable,
+            // increase size of this icon
+            contentPadding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+            leading: const Icon(Icons.person),
+            title: Text(friend['name'],
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+            onTap: () {
+              setState(() => friend['val'] = !friend['val']);
+              if (friend['val'])
+                group_users[friend['id']] = friend['name'];
+              else
+                group_users.remove(friend['id']);
+              // print(group_users);
+            },
+          ),
+        ),
+      );
 }
-
